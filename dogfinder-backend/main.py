184157d6b0c22a89643b004466508c99b98cb6f2
Facundo Almaraz <facundoalmaraz8@gmail.com>
2025-06-14 07:19:44 -0300

    cambios

--- dogfinder-backend/main.py
+++ dogfinder-backend/main.py
@@ -1,25 +1,22 @@
 from fastapi import FastAPI, File, UploadFile, Form
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles  # ⬅️ nuevo
+from fastapi.staticfiles import StaticFiles
 from scripts.combined_fuzzy import process_image_and_criteria
 import shutil
 import os
 
 app = FastAPI()
 
-# CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "https://3xdwdjjr-3000.brs.devtunnels.ms"],
+    allow_origins=["http://localhost:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-# Montar carpeta 'dataset' como pública
-app.mount("/dataset", StaticFiles(directory="dataset"), name="dataset")  # ⬅️ nuevo
+app.mount("/dataset", StaticFiles(directory="dataset"), name="dataset")
 
-# Carpeta para subir imágenes temporales
 UPLOAD_FOLDER = "uploads"
 os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
@@ -63,5 +60,5 @@ async def auto_analyze(image: UploadFile = File(...)):
         "size": traits["size"],
         "age": traits["age"],
         "color": color,
-        "hasCollar": False  # se puede mejorar con visión
+        "hasCollar": False
     }
